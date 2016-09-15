7f0836377574a5aefbed318a7ffb3c64dee0c499
lin <java.com.lv@163.com>
2016-09-15 17:28:42 +0800

    update

--- src/main/webapp/test/index.js
+++ src/main/webapp/test/index.js
@@ -32,13 +32,31 @@ function httpGet(url,callback) {
 function render(){
     var url = "http://" + location.host + "/venues/getAreas";
     var count = 60000;
+    //setTimeout(function () {
+    //    document.getElementById("all_view").addEventListener('click', function (evt) {
+    //        zOut();
+    //    })
+    //},1000);
+
     httpGet(url,function (data1) {
         data1.forEach(function (value) {
+
+            var rc = value.rc.split("|").join(" ");
+            var polygon = document.createElementNS(SVG_NS, "polygon");
+            polygon.setAttribute("points", rc);
+            polygon.setAttribute("id",value.standId);
+            polygon.addEventListener('click',function(){
+                zOut(polygon)
+
+            });
+            document.getElementById("all_view").appendChild(polygon);
+
             url = "http://" + location.host + "/venues/getSeats?standId=" + value.standId;
             httpGet(url,function(data) { //resolve的回调
                 if(!data){
                     return;
                 }
+
                 data.forEach(function (o, index) {
 
                     var obj = create();
@@ -48,6 +66,7 @@ function render(){
                     obj.setAttribute('x', x);
                     obj.setAttribute('y', y);
                     if(count-->0){
+
                         document.getElementById("all_view").appendChild(obj);
 
                     }
@@ -55,6 +74,7 @@ function render(){
             });
         })
     })
+
 }
 
 render();
@@ -84,4 +104,48 @@ function zoomIn() {
     ts[0] = ts[0] * 0.8 ;
     ts[1] = ts[1] * 0.8 ;
     document.getElementById("all_view").setAttribute('transform','scale('+ts[1]+','+ts[1]+')');
+}
+
+
+//drap drop
+function allowDrop(ev)
+{
+    ev.preventDefault();
+}
+
+function drag(ev)
+{
+    ev.dataTransfer.setData("Text",ev.target.id);
+    //document.getElementById("svg-view").style.display = 'none';
+}
+
+function drop(ev)
+{
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
+var scale = 1.0;
+function zOut(obj){
+    var value = 3.0;
+    scale = scale * value;
+    var svg_view = document.getElementById("svg-view");
+    var width = svg_view.getAttribute("width");
+    var height = svg_view.getAttribute("height");
+
+    width = width * value;
+    height = height * value;
+    svg_view.setAttribute("width" , width);
+    svg_view.setAttribute("height" , height);
+
+
+    var box = obj.getBBox();
+    var x = (box.x ) * scale;
+    var y = (box.y ) * scale;
+    console.log(width,height,x,y);
+
+    document.getElementById('scroll-div').scrollTop = y;
+    document.getElementById('scroll-div').scrollLeft = x;
 }
